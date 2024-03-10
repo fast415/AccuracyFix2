982de60214dffa9db42c0d994c83a09a1945b19d
thinkk <50614694+think4k@users.noreply.github.com>
2024-03-10 12:02:36 +0100

    Update AccuracyFix.h

--- AccuracyFix/AccuracyFix.h
+++ AccuracyFix/AccuracyFix.h
@@ -5,10 +5,7 @@ class CAccuracyFix
 public:
 	void ServerActivate();
 
-	void CmdEnd(const edict_t* pEdict);
 	void TraceLine(const float* vStart, const float* vEnd, int fNoMonsters, edict_t* pentToSkip, TraceResult* ptr);
-    void PostThink(CBasePlayer* Player);
-
 private:
 	cvar_t* m_af_distance_all;
 	std::array<cvar_t*, MAX_WEAPONS + 1> m_af_distance;
